73cda2b4eebde7c3853f1f6437f9a65d56f00cad
irssaini <contact.ramindersingh@gmail.com>
2022-10-07 23:32:43 +0530

    Second commit

--- src/test/java/SeleniumWebdriver/SeleniumWebdriver/TC_Xpath_Locator.java
+++ src/test/java/SeleniumWebdriver/SeleniumWebdriver/TC_Xpath_Locator.java
@@ -1,15 +1,19 @@
 package SeleniumWebdriver.SeleniumWebdriver;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TC_Xpath_Locator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","C:\\Data\\RSSaini\\x\\SeleniumWebdriver\\Drivers\\chromedriver.exe");
-		
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Data\\RSSaini\\x\\SeleniumWebdriver\\Drivers\\chromedriver.exe");
+
 		WebDriver driver = new ChromeDriver();
 
 		// open sauce demo web page
@@ -24,6 +28,15 @@ public class TC_Xpath_Locator {
 		// Locate Login button
 		driver.findElement(By.className("submit-button")).click();
 
+		// Switch to product page
+		String currWindowshandle = driver.getWindowHandle();
+		driver.switchTo().window(currWindowshandle);
+
+		// driver.findElement(By.linkText("Sauce Labs Backpack")).click(); //Click on product name
+		//driver.findElement(By.partialLinkText("Bike")).click();
+
+		List <WebElement> elementsList = driver.findElements(By.partialLinkText("Sauce"));
+		System.out.println("Element Size : "+ elementsList.size());
 	}
 
 }
