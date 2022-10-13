141d674f641ae6547a27fe8ea17a7767c56c2ad6
irssaini <contact.ramindersingh@gmail.com>
2022-10-13 19:12:37 +0530

    chrome lauch using dependencies

--- src/test/java/SeleniumWebdriver/SeleniumWebdriver/FirstTestCase.java
+++ src/test/java/SeleniumWebdriver/SeleniumWebdriver/FirstTestCase.java
@@ -1,28 +1,29 @@
 package SeleniumWebdriver.SeleniumWebdriver;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FirstTestCase {
 
 	public static void main(String[] args) {
-			// TODO Auto-generated method stub
-	//lauch chrome browser
-			System.setProperty("webdriver.chrome.driver", "C:\\Data\\Selenium\\chromedriver_win32 (4)\\chromedriver.exe");
-			WebDriver driver =new ChromeDriver();
-			//Launch google.in
-			driver.navigate().to("https://www.google.co.in/");
-			
-			//capture tittle of webpage and print
-			String tittle = driver.getTitle();
-			System.out.println("Page Tittle : "+ tittle);
-			
-			//capture url of page
-			System.out.println("URL : "+ driver.getCurrentUrl());
-			
-			//Capture page source
-			System.out.println("Page Source : "+ driver.getPageSource());
-		}
+		// TODO Auto-generated method stub
+		// lauch chrome browser
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		ChromeDriver driver = new ChromeDriver(options);
 
-	}
+		// Launch google.in
+		driver.navigate().to("https://www.google.co.in/");
 
+		// capture tittle of webpage and print
+		String tittle = driver.getTitle();
+		System.out.println("Page Tittle : " + tittle);
+
+		// capture url of page
+		System.out.println("URL : " + driver.getCurrentUrl());
+
+		// Capture page source
+		System.out.println("Page Source : " + driver.getPageSource());
+	}}
--- src/test/java/SeleniumWebdriver/SeleniumWebdriver/TC_Xpath_Locator.java
+++ src/test/java/SeleniumWebdriver/SeleniumWebdriver/TC_Xpath_Locator.java
@@ -6,15 +6,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TC_Xpath_Locator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Data\\RSSaini\\x\\SeleniumWebdriver\\Drivers\\chromedriver.exe");
-
-		WebDriver driver = new ChromeDriver();
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		WebDriver driver = new ChromeDriver(options);
 
 		// open sauce demo web page
 		driver.get("https://www.saucedemo.com/");
@@ -32,11 +34,12 @@ public class TC_Xpath_Locator {
 		String currWindowshandle = driver.getWindowHandle();
 		driver.switchTo().window(currWindowshandle);
 
-		// driver.findElement(By.linkText("Sauce Labs Backpack")).click(); //Click on product name
-		//driver.findElement(By.partialLinkText("Bike")).click();
+		// driver.findElement(By.linkText("Sauce Labs Backpack")).click(); //Click on
+		// product name
+		// driver.findElement(By.partialLinkText("Bike")).click();
 
-		List <WebElement> elementsList = driver.findElements(By.partialLinkText("Sauce"));
-		System.out.println("Element Size : "+ elementsList.size());
+		List<WebElement> elementsList = driver.findElements(By.partialLinkText("Sauce"));
+		System.out.println("Element Size : " + elementsList.size());
 	}
 
 }
